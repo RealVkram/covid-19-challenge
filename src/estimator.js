ada3dc71b67a7492f69db82997a17f5df49b7f2d
RealVkram <ajayiadegbemi@gmail.com>
2020-04-10 17:46:01 +0100

    added the estimate property

--- src/estimator.js
+++ src/estimator.js
@@ -12,7 +12,6 @@ const inputData = {
   totalHospitalBeds: 678874
 };
 
-
 let factor;
 
 const covid19ImpactEstimator = (data) => {
@@ -37,49 +36,51 @@ const covid19ImpactEstimator = (data) => {
   }
 
   const outputData = {
-    data, // the input data you got
-    impact: {
-      currentlyInfected: reportedCases * 10,
-      infectionsByRequestedTime: reportedCases * 10 * factor,
-      severeCasesByRequestedTime: 0.15 * reportedCases * 10 * factor,
-      hospitalBedsByRequestedTime: Math.floor(
-        0.35 * totalHospitalBeds - 0.15 * reportedCases * 10 * factor
-      ),
-      casesForICUByRequestedTime: 0.05 * reportedCases * 10 * factor,
-      casesForVentilatorsByRequestedTime: Math.floor(
-        0.02 * reportedCases * 10 * factor
-      ),
-      dollarsInFlight: Math.floor(
-        reportedCases
-          * 10
-          * factor
-          * avgDailyIncomePopulation
-          * avgDailyIncomeInUSD
-          * timeToElapse
-      )
-    }, // your best case estimation
-    severeImpact: {
-      currentlyInfected: reportedCases * 50,
-      infectionsByRequestedTime: reportedCases * 50 * factor,
-      severeCasesByRequestedTime: 0.15 * (reportedCases * 50 * factor),
-      hospitalBedsByRequestedTime: Math.floor(
-        0.35 * totalHospitalBeds - 0.15 * reportedCases * 50 * factor
-      ),
-      casesForICUByRequestedTime: Math.floor(
-        0.05 * (reportedCases * 50 * factor)
-      ),
-      casesForVentilatorsByRequestedTime: Math.floor(
-        0.02 * (reportedCases * 50 * factor)
-      ),
-      dollarsInFlight: Math.floor(
-        reportedCases
-          * 50
-          * factor
-          * avgDailyIncomePopulation
-          * avgDailyIncomeInUSD
-          * timeToElapse
-      ) // over a 30 days period.
-    } // your
+    data,
+    estimate: {
+      impact: {
+        currentlyInfected: reportedCases * 10,
+        infectionsByRequestedTime: reportedCases * 10 * factor,
+        severeCasesByRequestedTime: 0.15 * reportedCases * 10 * factor,
+        hospitalBedsByRequestedTime: Math.floor(
+          0.35 * totalHospitalBeds - 0.15 * reportedCases * 10 * factor
+        ),
+        casesForICUByRequestedTime: 0.05 * reportedCases * 10 * factor,
+        casesForVentilatorsByRequestedTime: Math.floor(
+          0.02 * reportedCases * 10 * factor
+        ),
+        dollarsInFlight: Math.floor(
+          reportedCases
+            * 10
+            * factor
+            * avgDailyIncomePopulation
+            * avgDailyIncomeInUSD
+            * timeToElapse
+        )
+      }, // your best case estimation
+      severeImpact: {
+        currentlyInfected: reportedCases * 50,
+        infectionsByRequestedTime: reportedCases * 50 * factor,
+        severeCasesByRequestedTime: 0.15 * (reportedCases * 50 * factor),
+        hospitalBedsByRequestedTime: Math.floor(
+          0.35 * totalHospitalBeds - 0.15 * reportedCases * 50 * factor
+        ),
+        casesForICUByRequestedTime: Math.floor(
+          0.05 * (reportedCases * 50 * factor)
+        ),
+        casesForVentilatorsByRequestedTime: Math.floor(
+          0.02 * (reportedCases * 50 * factor)
+        ),
+        dollarsInFlight: Math.floor(
+          reportedCases
+            * 50
+            * factor
+            * avgDailyIncomePopulation
+            * avgDailyIncomeInUSD
+            * timeToElapse
+        ) // over a 30 days period.
+      } // your
+    }
   };
 
   return outputData;
